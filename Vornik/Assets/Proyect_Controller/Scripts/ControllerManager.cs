511fe395bbb32cab6794cf30b5e90f3add8445af
danicas92 <al341512@uji..es>
2019-05-01 19:44:58 +0200

    Sistema de subtitulos implementado

---
+++ Vornik/Assets/Proyect_Controller/Scripts/ControllerManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControllerManager : MonoBehaviour
+{
+    static readonly float TIMER = 2f;
+
+    SubtitleControl subtitleControl;
+    PostProcessingControl postProcessingControl;
+
+
+    private void Awake()
+    {
+        subtitleControl = GetComponent<SubtitleControl>();
+        postProcessingControl = GameObject.Find("PostEffectPrefiles").GetComponent<PostProcessingControl>();
+        
+    }
+
+    void Start()
+    {
+        postProcessingControl.RestartValue();
+        StartCoroutine(StartScene());
+    }
+
+    IEnumerator StartScene()
+    {
+        yield return new WaitForSeconds(TIMER);
+        postProcessingControl.ActivateBlack(false);
+        yield return new WaitForSeconds(TIMER);
+        postProcessingControl.FocusChange(1);
+        yield return new WaitForSeconds(TIMER);
+        subtitleControl.ReadLine();
+        //Subtitulos
+    }
+    
+}
---
+++ Vornik/Assets/Proyect_Controller/Scripts/SubtitleControl.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+
+public class SubtitleControl : MonoBehaviour
+{
+    [Header("Entradas para la configuración de los subtitulos")]
+    [Tooltip("Array de strings para modificar de forma automática")]
+    public string[] texts;
+    [Tooltip("Array de audios para la modificación de forma automática")]
+    public AudioClip[] clips;
+    public int interactive;
+    public bool action = false;
+
+    public TextMeshProUGUI text;
+    public AudioSource audioSource;
+    int flag;
+
+    
+    void Start()
+    {
+        text.text = "";
+        flag = 0;
+    }
+
+    
+    void Update()
+    {
+        
+    }
+
+    public void ReadLine()
+    {
+        if (flag == interactive)
+        {
+            flag++;
+            ReadLine();
+        }//Accionar interaccion
+        else if (flag < texts.Length)
+            StartCoroutine(ReadLineSound());
+
+    }
+
+    IEnumerator ReadLineSound()
+    {
+        text.text = texts[flag];
+        audioSource.clip = clips[flag];
+        audioSource.Play();
+        yield return new WaitForSeconds(clips[flag].length);
+        flag++;
+        ReadLine();
+    }
+}
