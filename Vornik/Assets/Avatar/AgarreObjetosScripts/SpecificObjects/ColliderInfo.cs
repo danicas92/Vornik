e3d2b9a3ccc2efebca156b38117c886d55a9d3ce
danicas92 <al341512@uji..es>
2019-05-12 13:09:20 +0200

    Puzzle tetera

---
+++ Vornik/Assets/Avatar/AgarreObjetosScripts/SpecificObjects/ColliderInfo.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColliderInfo : MonoBehaviour
+{
+    [SerializeField] private int colliderIndentificador;
+
+    public int GetColliderIdentificador() { return colliderIndentificador; }
+}
---
+++ Vornik/Assets/Avatar/AgarreObjetosScripts/SpecificObjects/TrozoTetera.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrozoTetera : MonoBehaviour
+{
+    [SerializeField] private int identificador;
+
+    public int GetIdentificador() { return identificador; }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("TeapotPiece") && other.GetComponent<ColliderInfo>().GetColliderIdentificador() == identificador)
+        {
+            Debug.Log(other.GetComponentInParent<TrozoTetera>().GetIdentificador() +":"+  identificador);
+            if (other.GetComponentInParent<TrozoTetera>().GetIdentificador() > identificador) return;
+
+            if (!GetComponent<ObjectGrabber>().GetGrabbed() || !other.GetComponentInParent<ObjectGrabber>().GetGrabbed()) return;
+            
+            GetComponent<ObjectGrabber>().Throw(transform.position,transform.position,false);
+            Destroy(GetComponent<ObjectGrabber>());
+            Destroy(GetComponent<Rigidbody>());
+            transform.parent = other.transform;
+            transform.localPosition = Vector3.zero;
+            transform.localRotation = Quaternion.Euler(Vector3.zero);
+
+            RemoveCollsionWithFingers();
+        }
+        
+    }
+
+    private void RemoveCollsionWithFingers()
+    {
+        var fingersCollision = GetComponentsInChildren<Transform>();
+        foreach (var collisionGO in fingersCollision)
+        {
+            if (collisionGO.CompareTag("CollisionFingers"))
+            {
+                collisionGO.tag = "Untagged";
+            }
+        }
+    }
+}
