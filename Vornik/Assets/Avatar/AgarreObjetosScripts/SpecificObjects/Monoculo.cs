3f75bbb0870ecb1960de82632d332ea27b4ffe2b
danicas92 <al341512@uji..es>
2019-05-12 19:18:05 +0200

    Importación casa y monocular terminado

---
+++ Vornik/Assets/Avatar/AgarreObjetosScripts/SpecificObjects/Monoculo.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Monoculo : MonoBehaviour
+{
+    [SerializeField] private Camera camera;
+
+    private ObjectGrabber objectGrabber;
+
+    private void Awake()
+    {
+        objectGrabber = GetComponent<ObjectGrabber>();
+    }
+
+
+    private void Update()
+    {
+
+        if (objectGrabber.GetGrabbed() && !camera.enabled)
+        {
+            camera.enabled = true;
+            return;
+        }
+        else if(!objectGrabber.GetGrabbed())
+        {
+            camera.enabled = false;
+        }
+
+    }
+}
