6f51976aa2293dd1c7caf876db209730c9ba342e
danicas92 <al341512@uji..es>
2019-05-26 02:41:49 +0200

    MenuPrincipal

---
+++ Vornik/Assets/ButtonMenu.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using System;
+
+public class ButtonMenu : MonoBehaviour
+{
+    public enum TypeButton { NuevaPartida, Continuar, Creditos, Salir};
+    public TypeButton typeButton;
+    private AsyncOperation asyncOperation;
+
+    public void ActivateButton()
+    {
+        AsyncOperation operation;
+        switch (typeButton)
+        {
+            case TypeButton.NuevaPartida:
+                 operation = SceneManager.LoadSceneAsync(SceneManager.GetActiveScene().buildIndex + 1);
+                break;
+            case TypeButton.Continuar:
+                 operation = SceneManager.LoadSceneAsync(SceneManager.GetActiveScene().buildIndex + 1);//leer del archivo
+                break;
+            case TypeButton.Creditos:
+                operation = SceneManager.LoadSceneAsync(SceneManager.GetActiveScene().buildIndex + 1);//Poner escena de créditos
+                break;
+            case TypeButton.Salir:
+                Application.Quit();
+                break;
+        }
+    }
+}
---
+++ Vornik/Assets/MenuRay.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class MenuRay : MonoBehaviour
+{
+    public LayerMask layermask;
+    public TextMeshProUGUI text;
+    bool actived = false;
+    private string buttDer = "Oculus_CrossPlatform_Button2";
+
+    void FixedUpdate()
+    {
+        Ray ray = new Ray(Camera.main.transform.position, Camera.main.transform.forward);
+        RaycastHit rayCastHit;
+
+        if (Physics.Raycast(ray, out rayCastHit, Mathf.Infinity, layermask.value))
+        {
+            text = rayCastHit.collider.gameObject.GetComponent<TextMeshProUGUI>();
+            if (!actived)
+            {
+                text.gameObject.GetComponent<AudioSource>().Play();
+                text.fontStyle = FontStyles.Bold;
+            }
+            actived = true;
+        }
+        else
+        {
+            if(actived)
+                text.fontStyle = FontStyles.Normal;
+            actived = false;
+        }
+            
+
+        if (actived && Input.GetButtonDown(buttDer))
+        {
+            text.gameObject.GetComponent<ButtonMenu>().ActivateButton();
+        }
+        else
+            return;
+
+
+
+    }
+}
